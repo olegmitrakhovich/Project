b6dd73b85c6e5da50f343a1f31a86c0a6ffc9b44
Oleg Mitrakhovich <olegmit9@hotmail.com>
2021-07-15 19:21:16 -0400

    fixing filtered port issue

--- NmapProject.py
+++ NmapProject.py
@@ -23,11 +23,29 @@ print(os_results)
 
 print('============================================================')
 
+def open_ports():
+    variable = os_results[host]['ports'][i]['portid']
+    variable1 = os_results[host]['ports'][i]['state']
+    variable2 = os_results[host]['ports'][i]['service']['name']
+    variable3 = os_results[host]['ports'][i]['service']['version']
+    print ('[*] Discovered Port: ' + variable)
+    print ('[*] State : ' + variable1)
+    print ('[*] Service : ' + variable2)
+    print ('[*] Version : ' + variable3)
+    print ('====================================')
+
+def filtered_ports():
+    variable = os_results[host]['ports'][i]['portid']
+    variable1 = os_results[host]
+    variable2 = os_results[host]['ports'][i]['service']['name']
+    variable3 = "Filtered"
+    print ('[*] Discovered Port: ' + variable)
+    print ('[*] Service : ' + variable2)
+    print ('[*] State : ' + variable3)
+    print ('====================================')
+
 for i in range(len(os_results[host]['ports'])):
-        variable = os_results[host]['ports'][i]['portid']
-        variable2 = os_results[host]['ports'][i]['service']['name']
-        variable3 = os_results[host]['ports'][i]['service']['version']
-        print ('[*] Discovered Port: ' + variable)
-        print ('[*] Service : ' + variable2)
-        print ('[*] Version : ' + variable3)
-        print ('============================================================')
+    if os_results[host]['ports'][i]['state'] == 'open':
+        open_ports()
+    else:
+        filtered_ports()
