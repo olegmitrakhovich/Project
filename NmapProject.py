1f17c9b197a20124ec92942e03855641c6eaaa43
Oleg Mitrakhovich <olegmit9@hotmail.com>
2021-07-15 14:22:46 -0400

    testing arguments
    code by @greylupi

--- NmapProject.py
+++ NmapProject.py
@@ -1,11 +1,20 @@
 import nmap
 import json
+import argparse
+
+#importing nmap3 for python
 import sys
 sys.path.append('/root/python3-nmap')
 import nmap3
 
+parser = argparse.ArgumentParser()
+parser.add_argument("Host", help="enter a domain or IP address")
+args = parser.parse_args()
+
+host = sys.argv[1]
+
 nmap = nmap3.Nmap()
-os_results = nmap.nmap_version_detection("10.129.179.163")
+os_results = nmap.nmap_version_detection(host)
 print(os_results)
 
 #this pretty prints the json
@@ -14,10 +23,10 @@ print(jsonoutput)
 
 print('============================================================')
 
-for i in range(len(os_results['10.129.179.163']['ports'])):
-        variable = os_results['10.129.179.163']['ports'][i]['portid']
-        variable2 = os_results['10.129.179.163']['ports'][i]['service']['name']
-        variable3 = os_results['10.129.179.163']['ports'][i]['service']['version']
+for i in range(len(os_results[host]['ports'])):
+        variable = os_results[host]['ports'][i]['portid']
+        variable2 = os_results[host]['ports'][i]['service']['name']
+        variable3 = os_results[host]['ports'][i]['service']['version']
         print ('[*] Discovered Port: ' + variable)
         print ('[*] Service : ' + variable2)
         print ('[*] Version : ' + variable3)
